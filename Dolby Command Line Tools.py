9adb28a63a71f94e76d1a23f5d91c1a9810adfbf
jcoopertech <58943271+jcoopertech@users.noreply.github.com>
2024-05-09 22:13:59 +0100

    Update Dolby Command Line Tools.py
    option 2 and 3 first draft

--- Dolby Command Line Tools.py
+++ Dolby Command Line Tools.py
@@ -11,25 +11,58 @@ Written by coding@jcooper.tech
 
 Which option would you like:
 
-Option 1. Validate XML""")
-    """Option 2. Fix Dissolves in XML
-    Option 3. Update XML to latest DV Version
-    Option 4. Analyse a ProRes for HD10 Meta"""
+Option 1. Validate XML
+Option 2. Fix Dissolves in XML
+Option 3. Update XML to latest DV Version
+Option 4. Analyse a ProRes for HD10 Meta""")
     try:
         return int(input("Please choose [1/2/3/4] as a number only."))
     except:
         print("\n\n\n!!! >> You entered an invalid response")
         raise
 
-def option1():
-    userdirectory = str(input("Please enter complete directory path, including the final filename and extension: "))
-    result = subprocess.run(['/usr/local/bin/dolby_vision_professional_tools/metafier', '-validate', userdirectory], stdout=subprocess.PIPE)
+def option1(userdirectory):
+    result = subprocess.run(['/usr/local/bin/dolby_vision_professional_tools/metafier', '—validate', userdirectory], stdout=subprocess.PIPE)
     result.stdout
 
-    
+def option2(userdirectory):
+    result = subprocess.run(['/usr/local/bin/dolby_vision_professional_tools/metafier', '--fix-dissolves' userdirectory], stdout.subprocess.PIPE)
+    result.stdout
+
+def option3():
+    old = str(input("Input file path of XML to upgrade to latest DV Meta Version: "))
+    new = str(input("Input file name of updated XML (Don’t include .XML at the end): "))
+    new.append(".xml")
+    result = subprocess.run(['/usr/local/bin/dolby_vision_professional_tools/metafier', '-o', new, '--save-version latest', old])
+
+def option4():
+    print("pls b patientz")
+
+def motd():
+    print("""
+
+______      _ _                       _____  _____   _____           _     
+|  _  \    | | |              ____   |  ___||  _  | |_   _|         | |    
+| | | |___ | | |__  _   _    / __ \  |___ \ | |_| |   | | ___   ___ | |___ 
+| | | / _ \| | '_ \| | | |  / / _` |     \ \\____ |   | |/ _ \ / _ \| / __|
+| |/ / (_) | | |_) | |_| | | | (_| | /\__/ /.___/ /   | | (_) | (_) | \__ \
+|___/ \___/|_|_.__/ \__, |  \ \__,_| \____/ \____/    \_/\___/ \___/|_|___/
+                     __/ |   \____/                                        
+                    |___/                                                  """)
+
+
 if __name__ == "__main__":
+    motd()
     menuOpt = Menu()
+    fil = str(input("Please enter complete directory path, including the final filename and extension: "))
+
     if menuOpt == 1:
-        option1()
+        option1(fil)
+    elif menuOpt == 2:
+        option2(fil)
+    elif menuOpt == 3:
+        option3(fil)
+    elif menuOpt == 4:
+        option4(fil)
     input("Return to finish")
     input("Hit enter to close console")#
