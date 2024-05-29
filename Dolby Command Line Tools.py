f492fb63a23cdf98db1f2cb37faf299599397bf7
jcoopertech <58943271+jcoopertech@users.noreply.github.com>
2024-05-29 20:15:03 +0100

    Updates from meer32
    top level try/except clause is unreleased to F@59

--- Dolby Command Line Tools.py
+++ Dolby Command Line Tools.py
@@ -41,7 +41,7 @@ import subprocess
 def Menu():
     print("""
 Dolby Vision Tools Lines
-Written by coding@jcooper.tech
+Written by James Cooper - coding@jcooper.tech under the Guidance from Films@59 Post Productions
 
 This software provides no checks for filenames being correct, and assumes that DV Tools will do this.
 
@@ -51,17 +51,19 @@ Which option would you like:
 Option 1. Validate XML
 Option 2. Fix Dissolves in XML
 Option 3. Upgrade / Downgrade XML
-Option 4. Analyse a ProRes for HD10 Meta
-Option 5. Analyse a Full ProRes file""")
+Option 4. Analyse a ProRes for HDR10 Metadata File
+Option 5. Analyse a Full ProRes file for HDR10 Metadata File
+Option 6. Analyse a Tiff Image Sequence for HDR10 Metadata File
+Option 7. Analyse a Full ProRes 4444 file for HDR10 Metadata File""")
     
     try:
-        return int(input("Please choose [1/2/3/4/5] as a number only.: "))
+        return int(input("Please choose an option [1/2/3/4/5/6/7] as a number only.: "))
     except:
         print("\n\n\n!!! >> You entered an invalid response")
         raise
 
 def option1(userdirectory):
-    result = subprocess.run(['/usr/local/bin/dolby_vision_professional_tools/metafier', '—validate', userdirectory], stdout=subprocess.PIPE)
+    result = subprocess.run(['/usr/local/bin/dolby_vision_professional_tools/metafier', '-validate', userdirectory], stdout=subprocess.PIPE)
     result.stdout
 
 def option2(userdirectory):
@@ -105,20 +107,60 @@ def option5():
                              '--bda', in_file, xml_loc+".xml"], stdout=subprocess.PIPE)
     result.stdout
 
-if __name__ == "__main__":
-    menuOpt = Menu()
-    if menuOpt == 1 or menuOpt == 2:
-        fil = str(input("Please enter complete directory path, including the final filename and extension: "))
+def option6():
+    framerate = str(input("Please enter Frame Rate of Tiff Sequence: "))
+    startframe = str(input("Please enter starting frame number: "))
+    endframe = str(input("Please enter end frame number: "))
+    aspectratio = str(input("Please enter Aspect Ratio (e.g: 1.77778): "))
+    in_file = str(input("Please enter Tiff Image Sequence location (Replace File Frame Number with equivalent hash): "))
+    xml_loc = str(input("Please enter output xml file location >> Do not include extension, but include filename: "))
+    fr_concat = startframe+"-"+endframe
+    result = subprocess.run(['/usr/local/bin/dolby_vision_professional_tools/cm_analyze',\
+                             '-m', '21', \
+                             '-r', framerate, \
+                             '-f', fr_concat, \
+                             '--source-format', "bthdrfull", \
+                             '--aspect-ratios', aspectratio, aspectratio, \
+                             '--bda', in_file, xml_loc+".xml"], stdout=subprocess.PIPE)
+    result.stdout
 
-    if menuOpt == 1:
-        option1(fil)
-    elif menuOpt == 2:
-        option2(fil)
-    elif menuOpt == 3:
-        option3()
-    elif menuOpt == 4:
-        option4()
-    elif menuOpt == 5:
-        option5()
-    input("Return to finish")
-    input("Hit enter to close console")#
+def option7():
+    framerate = str(input("Please enter Frame Rate of ProRes file: "))
+    aspectratio = str(input("Please enter Aspect Ratio (e.g: 1.77778): "))
+    in_file = str(input("Please enter ProRes 4444 File location, include filename and extension: "))
+    xml_loc = str(input("Please enter output xml file location >> Do not include extension, but include filename: "))
+    result = subprocess.run(['/usr/local/bin/dolby_vision_professional_tools/cm_analyze',\
+                             '-m', '21', \
+                             '-r', framerate, \
+                             '--source-format', "u10 p444 lsb32rev le 444 ycbcr_bt2020 computer pq bt2020", \
+                             '--aspect-ratios', aspectratio, aspectratio, \
+                             '--bda', in_file, xml_loc+".xml"], stdout=subprocess.PIPE)
+    result.stdout
+
+try:
+    if __name__ == "__main__":
+        menuOpt = Menu()
+        if menuOpt == 1 or menuOpt == 2:
+            fil = str(input("Please enter complete directory path, including the final filename and extension: "))
+            
+        if menuOpt == 1:
+            option1(fil)
+        elif menuOpt == 2:
+            option2(fil)
+        elif menuOpt == 3:
+            option3()
+        elif menuOpt == 4:
+            option4()
+        elif menuOpt == 5:
+            option5()
+        elif menuOpt == 6:
+            option6()
+        elif menuOpt == 7:
+            option7()
+
+        input("Return to finish")
+        input("Hit enter to close console")
+
+except:
+    raise
+    input("did an oopsie")
